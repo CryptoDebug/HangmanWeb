ebb307105aafd4ae92af56ec031ce7e8646c6cc0
CryptoDebug <axel10.lemaitre@gmail.com>
2023-11-27 10:02:30 +0100

    intitialization

---
+++ hangman/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+
+    "net/http"
+    "text/template"
+
+)
+
+func main() {
+
+        http.HandleFunc("/", index)
+        http.HandleFunc("/game", game)
+        http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+        http.ListenAndServe(":8080", nil)
+    }
+
+    func index(w http.ResponseWriter, r http.Request) {
+        tIndex, err := template.ParseFiles("./templates/index.html")
+        if err != nil {
+            panic(err)
+        }
+        tIndex.Execute(w, nil)
+    }
+
+    func game(w http.ResponseWriter, rhttp.Request) {
+        tGame, err := template.ParseFiles("game.html")
+        if err != nil {
+            panic(err)
+        }
+        tGame.Execute(w, nil)
+    }
